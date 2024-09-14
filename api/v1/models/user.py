5f72c4dc0f17bb608b734c64e0c532a8b1d15548
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-09-14 16:33:17 +0100

    refactor: clean user model of bloat

--- api/v1/models/user.py
+++ api/v1/models/user.py
@@ -1,9 +1,8 @@
 """User data model"""
 
-from sqlalchemy import Column, String, Boolean, DateTime, func
+from sqlalchemy import Column, String, Boolean
 from sqlalchemy.orm import relationship
 from api.v1.models.base_model import BaseTableModel
-from datetime import datetime, timedelta, timezone
 
 
 class User(BaseTableModel):
@@ -13,8 +12,6 @@ class User(BaseTableModel):
     password = Column(String, nullable=True)
     first_name = Column(String)
     last_name = Column(String)
-    is_superadmin = Column(Boolean, server_default="false")
-    is_deleted = Column(Boolean, server_default="false")
 
     profile = relationship("Profile", back_populates="user", uselist=False)
     activity_logs = relationship("ActivityLog", back_populates="user")
@@ -22,9 +19,7 @@ class User(BaseTableModel):
     def to_dict(self):
         obj_dict = super().to_dict()
         obj_dict.pop("password")
-        if self.last_login:
-            obj_dict["last_login"] = self.last_login.isoformat()
         return obj_dict
 
     def __str__(self):
-        return self.email
+        return "User: {} {}".format(self.email, self.first_name)
