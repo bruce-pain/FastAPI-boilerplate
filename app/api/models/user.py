37908e29194596ec31c92a8b68e5059fe00e8a86
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-11-15 22:09:59 +0100

    refactor: modify models to exclude unnecessary functions

--- app/api/models/user.py
+++ app/api/models/user.py
@@ -1,22 +1,14 @@
 """User data model"""
 
-from sqlalchemy import Column, String, Boolean
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 from app.core.base.model import BaseTableModel
 
 
 class User(BaseTableModel):
     __tablename__ = "users"
 
-    email = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=True)
-    first_name = Column(String)
-    last_name = Column(String)
-
-    def to_dict(self):
-        obj_dict = super().to_dict()
-        obj_dict.pop("password")
-        return obj_dict
 
     def __str__(self):
-        return "User: {} {}".format(self.email, self.first_name)
+        return "User: {}".format(self.username)
