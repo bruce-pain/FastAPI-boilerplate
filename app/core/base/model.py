222bc2eca0c719be8401ab27214c4ba31a020ed0
Franklin Ikeh <ikehfranklin06@gmail.com>
2026-05-18 18:48:41 +0100

    refactor(models): use mapped_column instead of Column

--- app/core/base/model.py
+++ app/core/base/model.py
@@ -1,8 +1,12 @@
 """This is the Base Model Class"""
 
+from datetime import datetime
+
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.orm import Mapped, mapped_column
 from uuid_extensions import uuid7
+
 from app.core.database import Base
-from sqlalchemy import Column, String, DateTime, func
 
 
 class BaseTableModel(Base):
@@ -10,8 +14,12 @@ class BaseTableModel(Base):
 
     __abstract__ = True
 
-    id = Column(String, primary_key=True, index=True, default=lambda: str(uuid7()))
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(
+    id: Mapped[str] = mapped_column(
+        String, primary_key=True, index=True, default=lambda: str(uuid7())
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
--- app/features/auth/models.py
+++ app/features/auth/models.py
@@ -1,6 +1,7 @@
 """User data model"""
 
-from sqlalchemy import Column, String
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.base.model import BaseTableModel
 
@@ -8,8 +9,9 @@ from app.core.base.model import BaseTableModel
 class User(BaseTableModel):
     __tablename__ = "users"
 
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=True)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=True)
 
     def __str__(self):
         return "User: {}".format(self.email)
+
